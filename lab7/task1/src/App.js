5e76e904718103132577dd17da172a7ca24d5870
Pavlo Pavlichenko <pavlo.pavlichenko@symphony-solutions.com>
2022-11-27 23:32:23 +0200

    n

--- lab7/task1/src/App.js
+++ lab7/task1/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React task1
+          Learn React Task 1
         </a>
       </header>
     </div>
